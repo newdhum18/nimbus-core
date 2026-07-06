8a6bb7a0d371c6e13f6798886cdb672d7a44dc5f
newdhum18 <newdhum18@gmail.com>
2026-07-06 19:03:59 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const NIMBUS_CONFIG = {
-  appName: "Nimbus Core",
-  realProjectName: "Mega Hunter Pro Monitor",
-  temporaryPin: "775224",
-  version: "0.2.0-flat",
-  apiBase: "",
-  searchProfiles: ["US", "UK", "JP", "BR", "DE"],
-  notes: "Flat starter for iPhone GitHub upload. Real Cloudflare Access, Brave API, and D1 will be added later."
-};
