6698013523cc37bb6cf1b79ca42b20f09487017b
newdhum18 <newdhum18@gmail.com>
2026-07-08 19:21:20 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,3 +0,0 @@
-const CACHE='nimbus-v27-rewrite-static-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/style.css','/app.js','/config.js','/manifest.json']))));
-self.addEventListener('fetch',e=>{ if(new URL(e.request.url).pathname.startsWith('/api/')) return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
